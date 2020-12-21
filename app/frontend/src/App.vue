3def6361991b009d6d16c1b9d3e5e6ae4fa56a7a
ristemingov <riste.mingov@gmail.com>
2020-12-21 22:33:49 +0100

    Initial setup

--- app/frontend/src/App.vue
+++ app/frontend/src/App.vue
@@ -1,7 +1,9 @@
 <template>
-  <!-- <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-  <router-link to="/">Home</router-link>
-  <router-link to="/about">About</router-link>
-  <router-view></router-view>
+  <div>
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+  </div>
+  <div>
+    <router-view></router-view>
+  </div>
 </template>
