b98d2aeabc8cb469545d11cae1ca0f28d74beae5
ristemingov <riste.mingov@gmail.com>
2020-12-21 22:22:55 +0100

    Frontend setup

---
+++ app/frontend/src/main.js
@@ -0,0 +1,17 @@
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import App from './App.vue';
+import Home from './components/Home.vue';
+import About from './components/About.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', component: Home },
+    { path: '/about', component: About },
+  ],
+});
+
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
